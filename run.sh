df2dfa82b1a1391a9ec8a187bc6df3c062e05bfa
saad <44510725+saadkh1@users.noreply.github.com>
2024-04-25 04:28:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ sleep 10
 
 docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/main.py -d
 
-docker-compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic $TOPIC_NAME --replication-factor 1 --partitions 1 --bootstrap-server 172.18.0.5:9092
-echo "topic $TOPIC_NAME was created"
+docker-compose exec kafka /bin/bash -c "sleep 10 && /kafka-setup.sh"

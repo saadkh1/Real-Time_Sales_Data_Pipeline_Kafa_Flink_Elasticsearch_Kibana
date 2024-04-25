8823404866b141860bc71878742fdcbc5a8eecf7
saad <44510725+saadkh1@users.noreply.github.com>
2024-04-25 05:05:50 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,10 +6,10 @@ docker-compose up -d
 echo Waiting for services to initialize (sleeping for 10 seconds)...
 timeout /t 10 > nul
 
-echo Executing Flink job...
-docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/main.py -d
-
 echo Create Kafka topic...
 docker-compose exec kafka /bin/bash -c "sleep 10 && /kafka-setup.sh"
+
+echo Executing Flink job...
+docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/main.py -d
 
 echo Script execution completed.

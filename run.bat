ac28804c0e1ff3c47c91c4ed501f75064016ddf7
saad <44510725+saadkh1@users.noreply.github.com>
2024-04-25 04:04:33 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,8 +9,4 @@ timeout /t 10 > nul
 echo Executing Flink job...
 docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/main.py -d
 
-echo Create Kafka topic...
-docker-compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic %TOPIC_NAME% --replication-factor 1 --partitions 1 --bootstrap-server 172.18.0.5:9092
-echo "topic %TOPIC_NAME% was created"
-
 echo Script execution completed.
